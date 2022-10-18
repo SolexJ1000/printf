3a2a542e36badfa534b56e116ec92c298c33b240
SolexJ1000 <joseph.appolos60@gmail.com>
2022-10-18 15:15:37 -0700

    Mreged and compiled the functions

--- print_rot.c
+++ print_rot.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * print_rev - writes the str in reverse
+ * print_rot - writes the str in ROT13
  * @arguments: input string
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
  * Return: number of chars printed.
  */
-int print_rev(va_list arguments, char *buf, unsigned int ibuf)
+
+int print_rot(va_list arguments, char *buf, unsigned int ibuf)
 {
+	char alf[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	char *str;
-	unsigned int i;
-	int j = 0;
-	char nill[] = "(llun)";
+	unsigned int i, j, k;
+	char nill[] = "(avyy)";
 
 	str = va_arg(arguments, char *);
 	if (str == NULL)
@@ -22,11 +24,18 @@ int print_rev(va_list arguments, char *buf, unsigned int ibuf)
 		return (6);
 	}
 	for (i = 0; str[i]; i++)
-		;
-	j = i - 1;
-	for (; j >= 0; j--)
 	{
-		ibuf = handl_buf(buf, str[j], ibuf);
+		for (k = j = 0; alf[j]; j++)
+		{
+			if (str[i] == alf[j])
+			{
+				k = 1;
+				ibuf = handl_buf(buf, rot[j], ibuf);
+				break;
+			}
+		}
+		if (k == 0)
+			ibuf = handl_buf(buf, str[i], ibuf);
 	}
 	return (i);
 }
